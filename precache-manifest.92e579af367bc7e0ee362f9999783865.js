aa639d808a9d0c48045733e8869f1650ff291c29
alexeysurmach <flashhome1985@gmail.com>
2019-10-26 13:08:08 +0300

    Updates

---
+++ precache-manifest.92e579af367bc7e0ee362f9999783865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4cb8c2f28beb52e7b2c66555048141",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2573551d2c91b306f2cd",
+    "url": "/portfolio/static/css/main.0e42ceae.chunk.css"
+  },
+  {
+    "revision": "e9dd97f83efa20a58fe4",
+    "url": "/portfolio/static/js/2.284d780e.chunk.js"
+  },
+  {
+    "revision": "2573551d2c91b306f2cd",
+    "url": "/portfolio/static/js/main.1891fb3f.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
